a4841c0c407d117a0e5968d4ca176b8fc1f1af56
Timur <b.a.timur.exe@gmail.com>
2023-04-02 14:45:36 +0400

    01_Java&txt_codefiles_added

---
+++ two_random_numbers_petproject03.java
@@ -0,0 +1,53 @@
+package IsmayilMath;
+
+import java.util.Scanner;
+
+
+
+public class Main {
+
+    public static void main(String[] args)  {
+
+        Scanner console = new Scanner(System.in);
+        double firstnum;
+        double secondnum;
+        while (console.hasNextLine()) {
+
+            var x = console.nextLine();
+            var y = console.nextLine();
+
+
+
+            if(isNumeric(x)){
+                firstnum = Double.parseDouble(x);
+                firstnum = firstnum%2==0? firstnum*2 : firstnum+2;
+
+            }else {
+                System.out.println("Please input two random numbers :-)");
+                continue;
+            }
+
+            if(isNumeric(y)){
+                secondnum  = Double.parseDouble(y);
+                secondnum  = secondnum%2==0? secondnum*2 : secondnum+2;
+            }else {
+                System.out.println("Please input two random  number :-)");
+                continue;
+            }
+
+            System.out.println(firstnum +secondnum);
+
+            }
+
+        }
+
+
+    public static boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch(NumberFormatException e){
+            return false;
+        }
+    }
+}
